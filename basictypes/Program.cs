4a4f6ca15b3afa7784d1d7183a6ef03b9e955164
MarcosLudgerio <raimundo.ludgerio@virtus.ufcg.edu.br>
2021-12-13 12:51:34 -0300

    first steps in new language programming

---
+++ basictypes/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicTypes; // Agrupa as classes que estão dentro desse espaço (semelhante aos pacotes em Java)
+
+class TestingEverything
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
