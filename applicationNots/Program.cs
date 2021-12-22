c8aa4ea909b54d94a0237efcf5de67c03869615e
MarcosLudgerio <raimundo.ludgerio@virtus.ufcg.edu.br>
2021-12-22 10:53:22 -0300

    feat: added struct student for calculation notes

--- applicationNots/Program.cs
+++ applicationNots/Program.cs
@@ -1,37 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Informe a opção desejada:");
-Console.WriteLine("1 - Cadastrar novo aluno");
-Console.WriteLine("2 - Listar alunos");
-Console.WriteLine("3 - Calcular média geral");
-Console.WriteLine("X - Parar a aplicação");
-Console.WriteLine("");
-
-string option = Console.ReadLine();
-string[] options = new string[4] { "1", "2", "3", "X" };
-
-while (option.ToUpper() != "X")
+﻿namespace ApplicationNots
 {
-    switch (option)
+    public class Application
     {
-        case "1":
-            //TODO: Add student
-            Console.WriteLine("Esclheu a primeira opção");
-            break;
+        public static void Main(string[] args)
+        {
+            string[] options = new string[4] { "1", "2", "3", "X" };
+            Student[] students = new Student[5];
+            string option = ShowOptions();
+            while (option.ToUpper() != "X")
+            {
+                switch (option)
+                {
+                    case "1":
+                        Student student = new Student();
+                        Console.WriteLine("Adicionar novo aluno");
+                        Console.Write("Informe o nome do aluno: ");
+                        student.Name = Console.ReadLine();
+                        Console.Write("Informe a note do aluno: ");
+                        student.Note = Decimal.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        break;
 
-        case "2":
-            //TODO: list student
-            Console.WriteLine("Esclheu a primeira opção");
-            break;
+                    case "2":
+                        //TODO: list student
+                        Console.WriteLine("Esclheu a primeira opção");
+                        break;
 
-        case "3":
-            //TODO: avg
-            Console.WriteLine("Esclheu a primeira opção");
-            break;
+                    case "3":
+                        //TODO: avg
+                        Console.WriteLine("Esclheu a primeira opção");
+                        break;
 
-        default:
-            Console.WriteLine($"Opcão {option} é inválida, tente novamente");
-            break;
+                    default:
+                        Console.WriteLine($"Opcão {option} é inválida, tente novamente");
+                        break;
+                }
+                option = ShowOptions();
+            }
+            string ShowOptions()
+            {
+                Console.WriteLine("Informe a opção desejada:");
+                Console.WriteLine("1 - Cadastrar novo aluno");
+                Console.WriteLine("2 - Listar alunos");
+                Console.WriteLine("3 - Calcular média geral");
+                Console.WriteLine("X - Parar a aplicação");
+                string option = Console.ReadLine();
+                Console.WriteLine();
+                return option;
+            }
+        }
     }
-    option = Console.ReadLine();
 }
-
---
+++ applicationNots/Student.cs
@@ -0,0 +1,9 @@
+namespace ApplicationNots
+{
+    public struct Student
+    {
+        public string Name { get; set; }
+        public decimal Note { get; set; }
+    }
+
+}
